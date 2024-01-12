f8f96717ee373f423f0778b454d6692e994e53ca
Heribio <ethan.levratti@icloud.com>
2024-01-12 17:37:09 +0100

    fetch backend url

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function onMessageHandler(target, userMessage, self) {
     return; // Ignore messages from the bot
   }
 
+  // Display word in the chat
+  const chatContainer = document.getElementById('chat-box-content');
+  chatContainer.textContent = chatContainer.textContent + userMessage + '\n';
+
   // If the command is known, let's execute it
   if (userMessage.length === 1 && isNaN(userMessage)) {
     const wordContainer = document.getElementById('word-display');
@@ -18,6 +22,17 @@ function onMessageHandler(target, userMessage, self) {
     } else {
       console.log(`* Letter is not in the word: ${userMessage}`);
     }
+
+    // Fetch messages from the backend and update chat-box-content
+    fetch('http://localhost:5000/twitch')
+      .then(console.log('Success!'))
+      .then(response => response.json())
+      .then(data => {
+        // Update chat-box-content with received messages
+        chatContainer.textContent = chatContainer.textContent + data.content + '\n';
+        console.log(`* Received messages: ${data.content}`);
+      })
+      .catch(error => console.error('Error fetching messages:', error));
   } else {
     console.log(`* Ignoring message: ${userMessage}`);
   }
